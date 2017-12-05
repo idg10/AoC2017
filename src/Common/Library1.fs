3648de8961f8b72b836ccff3754bd50409397ad2
Ian Griffiths <ian@interact-sw.co.uk>
2017-12-05 06:58:53 +0000

    Day 5 star 1

--- src/Common/Library1.fs
+++ src/Common/Library1.fs
@@ -1,3 +1,10 @@
 ﻿module TextHandling
 
 let splitIntoRows (sheet : string) = sheet.Split([|'\n';'\r'|], System.StringSplitOptions.RemoveEmptyEntries)
+
+let getEmbeddedInput () =
+    let asm = System.Reflection.Assembly.GetEntryAssembly()
+    use reader = new System.IO.StreamReader(asm.GetManifestResourceStream("Input.txt"))
+    reader.ReadToEnd()
+
+let getEmbeddedRows () = getEmbeddedInput() |> splitIntoRows
---
+++ src/Day05/Program.fs
@@ -0,0 +1,38 @@
+﻿open Swensen.Unquote
+
+open TextHandling
+
+let execute (offsets : int seq) =
+    let offsetMap = offsets |> Seq.mapi (fun i v -> (i, v)) |> Map.ofSeq
+    Seq.unfold
+        (fun (pos, offsets : Map<int, int>) ->
+            if pos >= offsets.Count then None
+            else
+                let offset = offsets.[pos]
+                let nextPos = pos + offset
+                let result = (nextPos, Map.add pos (offset + 1) offsets)
+                in Some (result, result))
+        (0, offsetMap)
+
+let testInput = """
+0
+3
+0
+1
+-3"""
+
+let parseInput (rows : string seq) = Seq.map int rows
+
+let testOffsets = splitIntoRows testInput |> parseInput
+
+let stepCount offsets = execute offsets |> Seq.length
+
+let input = getEmbeddedRows () |> parseInput
+
+[<EntryPoint>]
+let main argv =
+    stepCount testOffsets =! 5
+
+
+    printfn "Part 1: %d" (stepCount input)
+    0 // return an integer exit code

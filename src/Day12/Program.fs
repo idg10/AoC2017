a8915182416ec6bc341f8258dfc034243aa04278
Ian Griffiths <ian@interact-sw.co.uk>
2017-12-12 10:45:31 +0000

    Day 12 star 2
    Changed how the solution to part 1 worked, to be able to get both answers. We now build up a list of all known groups as we go, merging them together if a line of input describes an element that connects two nodes we previously thought were separate.

--- src/Day12/Program.fs
+++ src/Day12/Program.fs
@@ -32,42 +32,89 @@ let testLines = testInput.Split([|'\r'; '\n'|], StringSplitOptions.RemoveEmptyEn
 
 let testConnections = parseLines testLines |> List.ofSeq
 
-let processGroup (groups: Map<int, Set<int>>) ((id, connections):(int * (int list))) =
-    let idsExplicitlyInThisGroup = Set.ofList (id :: connections)
+let processGroup
+    (groups: Map<int, Set<int>>, groupMembership: Map<int, int>)
+    ((id, connections): (int * (int list))) =
 
-    // Pick up all the ids
-    let idsForThisGroup =
+    // Go through the elements that this element is connected to, and work out which
+    // groups its connectees belong to, creating new groups for them if necessary
+    let (groups, currentlyKnownGroupsJoinedByThis, groupMembership) =
         connections
         |> Seq.fold
-            (fun (ids: Set<int>) connection ->
-                match Map.tryFind connection groups with
-                | Some idsFromConnection ->
-                        Set.union ids idsFromConnection
-                | None -> ids)
-            idsExplicitlyInThisGroup
+            (fun (groups: Map<int, Set<int>>, groupIds: Set<int>, groupMembership: Map<int, int>) connectedId ->
+                match Map.tryFind connectedId groupMembership with
+                | Some groupId ->
+                    // We've already seen this connectee, so just add the current
+                    // element's id to that conectee's group, and add the group
+                    // to the set of groups we're saying this item is connected to.
+                    let groupMembers =
+                        groups.[groupId]
+                        |> Set.add id
+                    (
+                        Map.add groupId groupMembers groups,
+                        Set.add groupId groupIds,
+                        groupMembership |> Map.add id groupId
+                    )
+                | None ->
+                    // We've never seen this connectee before, so add it to the
+                    // group for this element, creating that group if it doesn't
+                    // already exist.
+                    let groupIdForThisItem =
+                        match Map.tryFind id groupMembership with
+                        | Some existingGroupId -> existingGroupId
+                        | None -> id
+                    let groups =
+                        let items =
+                            match Map.tryFind groupIdForThisItem groups with
+                            | Some g -> Set.add connectedId g
+                            | None -> Set.ofList [groupIdForThisItem; connectedId]
+                        groups |> Map.add groupIdForThisItem items
+                    (
+                        groups,
+                        Set.add groupIdForThisItem groupIds,
+                        groupMembership
+                        |> Map.add id groupIdForThisItem
+                        |> Map.add connectedId groupIdForThisItem
+                    ))
+            (groups, Set.empty, groupMembership)
 
-    // Now that we've worked out the currently visible transitive closure of
-    // membership for this group, we need to go through all the ids we found
-    // and ensure that the target now also lists all the same ids.
-    // (E.g., if we just found that 3 is connected to 1, 2, and 5, we now need
-    // to make sure that 1, 2, and 5 all say they're connected to 1, 2, 3, and 5.
-    let groups =
-        idsForThisGroup
-        |> Seq.fold
-            (fun (groups: Map<int, Set<int>>) connection ->
-                let idsAlreadyInTarget =
-                    match Map.tryFind connection groups with
-                    | Some existingIds -> existingIds
-                    | None -> Set.empty
-                let updatedIdsForTarget = Set.union idsAlreadyInTarget idsForThisGroup
-                Map.add connection updatedIdsForTarget groups)
-            groups
 
-    Map.add id idsForThisGroup groups
+    // How many groups did we connect with? If it's just one, then we're already done -
+    // the previous step will have updated the maps correctly.
+    // However, if it turns out that this element's connections belong to multiple
+    // different groups, we now need to merge those into a single group.
+    let (groups, groupMembership) =
+        match List.ofSeq currentlyKnownGroupsJoinedByThis with
+        | [] -> failwith "Expecting to find or create at least one group"
+        | [groupId] -> (groups, groupMembership)
+        | groupIds ->
+            // This element connects to multiple existing groups, we
+            // need to merge them into one new group.
+            match List.sort groupIds with
+            | idToKeep::idsToMerge ->
+                idsToMerge
+                |> Seq.fold
+                    (fun (groups: Map<int, Set<int>>, groupMembership: Map<int, int>) (id: int) ->
+                        let targetGroup = groups.[idToKeep]
+                        let groupToMerge = groups.[id]
+                        let mergedGroup = Set.union targetGroup groupToMerge
+                        (
+                            groups
+                            |> Map.remove id
+                            |> Map.add idToKeep mergedGroup,
+                            groupToMerge
+                            |> Seq.fold
+                                (fun (groupMembership: Map<int, int>) idToMerge -> groupMembership |> Map.add idToMerge idToKeep)
+                                groupMembership
+                        ))
+                    (groups, groupMembership)
+            | _ -> failwith "Expecting list with at least two entries"
+
+    (groups, groupMembership)
 
 let findGroups (input: (int * (int list)) seq) =
     input
-    |> Seq.fold processGroup Map.empty
+    |> Seq.fold processGroup (Map.empty, Map.empty)
 
 
 let inputRows = getEmbeddedRows ()
@@ -80,7 +127,7 @@ let main argv =
     parseInputLine "1 <-> 1" =! (1, [1])
     parseInputLine "2 <-> 0, 3, 4" =! (2, [0; 3; 4])
 
-    let testGroups = findGroups testConnections
+    let (testGroups, groupMembership) = findGroups testConnections
     let group0 = testGroups.[0]
 
     Set.contains 0 group0 =! true
@@ -91,8 +138,10 @@ let main argv =
     Set.contains 6 group0 =! true
     Set.count group0 =! 6
 
-    let groupsFromInput = findGroups inputConnections
+    let (groupsFromInput, membershipFromInput) = findGroups inputConnections
     let group0 = groupsFromInput.[0]
 
     printfn "Part 1: %d" (Set.count group0)
+
+    printfn "Part 2: %d" (Map.count groupsFromInput)
     0

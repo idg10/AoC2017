5d72d5d834e6158d1eab78ac174eca1aaecfee27
Ian Griffiths <ian@interact-sw.co.uk>
2017-12-13 06:48:58 +0000

    Day 13 star 2

--- src/Day13/Program.fs
+++ src/Day13/Program.fs
@@ -13,15 +13,32 @@ let isScannerAtTopAtTick height tick =
     let positionInCycle = tick % cycleLength
     positionInCycle = 0
 
+let isCaughtForStartTime startTime (position, height) =
+    let tickWhenAtPosition = startTime + position
+    isScannerAtTopAtTick height tickWhenAtPosition
+
 let severity (heights: (int * int) seq) startTime =
+    let isCaught = isCaughtForStartTime startTime
     heights
     |> Seq.sumBy
         (fun (position, height) ->
-            let tickWhenAtPosition = startTime + position
-            let isCaught = isScannerAtTopAtTick height tickWhenAtPosition
-            if isCaught then position * height
+            if isCaught (position, height) then position * height
             else 0)
 
+// Can't just use severity to check whether we're caught, because being
+// caught at position 0 and nowhere else results in a severity of 0, but
+// you're still caught. The severity method cannot distinguish between
+// that case and not being caught.
+
+let isCaught (heights: (int * int) seq) startTime =
+    let isCaught = isCaughtForStartTime startTime
+    heights
+    |> Seq.exists isCaught
+
+let findFirstNonCaughtStartTime heights =
+    let isNotCaught = isCaught heights >> not
+    seq { 0..(System.Int32.MaxValue) }
+    |> Seq.find isNotCaught
 
 let pInputLine<'a> : Parser<int * int, 'a> =
     pipe2
@@ -55,4 +72,10 @@ let main argv =
     let heights = getEmbeddedRows () |> parseInput
     let severity = severity heights 0
     printfn "Part 1: %d" severity
+
+    let testFirstSafeTime = findFirstNonCaughtStartTime testHeights
+    testFirstSafeTime =! 10
+    let firstSafeTime = findFirstNonCaughtStartTime heights
+    printfn "Part 2: %d" firstSafeTime
+
     0

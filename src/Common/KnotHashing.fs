767ca81b5e525f309025faa066b0a3969a374617
Ian Griffiths <ian@interact-sw.co.uk>
2017-12-14 06:51:33 +0000

    Day 14 star 1

---
+++ src/Common/KnotHashing.fs
@@ -0,0 +1,95 @@
+﻿module KnotHashing
+
+type Step =
+    {
+        currentPosition: int
+        skipSize: int
+        elements: int list
+        elementCount: int   // Could calculate from elements, but that has O(N) for a list
+    }
+let makeInitialPosition elementCount =
+    {
+        currentPosition = 0
+        skipSize = 0
+        elements = [0..elementCount]
+        elementCount = elementCount
+    }
+
+let processMove previousStep length =
+    let elementsWithWrap = Seq.concat [previousStep.elements; previousStep.elements]
+
+    let wrappedElementsInTarget =
+        let excess = (previousStep.currentPosition + length) - previousStep.elementCount
+        if excess < 0 then 0 else excess
+    let reversedTargetSection =
+        elementsWithWrap
+        |> Seq.skip previousStep.currentPosition
+        |> Seq.take length
+        |> Seq.rev
+
+    let preWrapReversedTargetSection =
+        reversedTargetSection
+        |> Seq.take (length - wrappedElementsInTarget)
+
+    let postWrapReversedTargetSection =
+        reversedTargetSection
+        |> Seq.skip (length - wrappedElementsInTarget)
+
+    let initialSection =
+        Seq.append
+            postWrapReversedTargetSection
+            (Seq.take
+                (previousStep.currentPosition - wrappedElementsInTarget)
+                (Seq.skip wrappedElementsInTarget elementsWithWrap))
+
+    let sectionAfterTarget =
+        if wrappedElementsInTarget > 0 then Seq.empty
+        else
+            elementsWithWrap
+            |> Seq.skip (previousStep.currentPosition + length)
+            |> Seq.take (previousStep.elementCount - (previousStep.currentPosition + length))
+    {
+        currentPosition = (previousStep.currentPosition + length + previousStep.skipSize) % previousStep.elementCount
+        skipSize = previousStep.skipSize + 1
+        elements =
+            List.collect List.ofSeq [initialSection; preWrapReversedTargetSection; sectionAfterTarget]
+        elementCount = previousStep.elementCount
+    }
+
+let treatInputAsAscii (input : string) =
+    input
+    |> Seq.map int
+    |> List.ofSeq
+
+let convertInputToPart2List input =
+    List.append
+        (treatInputAsAscii input)
+        [17; 31; 73; 47; 23]
+
+
+let fullInitialPosition = makeInitialPosition 256
+
+let getSparseHash count inputText =
+    let lengths = convertInputToPart2List inputText
+    seq { 1..count }
+    |> Seq.fold
+        (fun (s: Step) _ -> Seq.fold processMove s lengths)
+        fullInitialPosition
+    |> (fun x -> x.elements)
+
+let makeDenseHash (blockSize: int) (sparseHash: int list) =
+    sparseHash
+    |> Seq.chunkBySize blockSize
+    |> Seq.map (Seq.fold (^^^) 0)
+    |> List.ofSeq
+    //|> Seq.map (fun block -> block |> Seq.fold (^^^) 0 )
+
+let denseHashToString (denseHash: int list) =
+    String.concat
+        ""
+        (denseHash |> Seq.map (fun v -> v.ToString("x2")))
+
+let hashString s =
+    let sparseHash = getSparseHash 64 s
+    let denseHash = makeDenseHash 16 sparseHash
+    denseHashToString denseHash
---
+++ src/Day14/Program.fs
@@ -0,0 +1,69 @@
+﻿open System
+open Swensen.Unquote
+
+open KnotHashing
+open TextHandling
+
+let toBinary (digits: int) (value: int) =
+    [1..digits]
+    |> Seq.fold
+        (fun (digits: bool list, v, digitsRemaining) _ ->
+            let digit = v % 2 = 1
+            (digit :: digits, v / 2, digitsRemaining - 1))
+        ([], value, digits)
+    |> fun (v, _, _) -> v
+ 
+let toBinary4 = toBinary 4
+
+let makeBinary (hexText: string) =
+    hexText
+    |> Seq.map (fun hexDigit -> Int32.Parse(string hexDigit, Globalization.NumberStyles.HexNumber) |> toBinary4)
+    |> Seq.concat
+let makeRow hashInput =
+    let hash = hashString hashInput
+    makeBinary hash
+
+let makeRowsForInput input =
+    [0..127]
+    |> Seq.map
+        (fun i ->
+            let text = sprintf "%s-%d" input i
+            makeRow text)
+
+
+let makeBinaryString t = 
+    makeBinary t |> Seq.map (fun b -> if b then '1' else '0') |> Array.ofSeq |> String;;
+
+
+let countUsed (grid: (bool seq) seq) =
+    grid
+    |> Seq.sumBy (Seq.sumBy (fun b -> if b then 1 else 0))
+
+[<EntryPoint>]
+let main argv =
+
+    makeBinaryString "a0c2017" =! "1010000011000010000000010111"
+    let testInput = "flqrgnkx"
+    let testRows = makeRowsForInput testInput
+    let croppedTestGrid =
+        testRows
+        |> Seq.map (Seq.take 8 >> List.ofSeq >> Seq.map (fun b -> if b then '#' else '.') >> Array.ofSeq >> String)
+        |> Seq.take 8
+        |> List.ofSeq
+    
+    croppedTestGrid.[0] =! "##.#.#.."
+    croppedTestGrid.[1] =! ".#.#.#.#"
+    croppedTestGrid.[2] =! "....#.#."
+    croppedTestGrid.[3] =! "#.#.##.#"
+    croppedTestGrid.[4] =! ".##.#..."
+    croppedTestGrid.[5] =! "##..#..#"
+    croppedTestGrid.[6] =! ".#...#.."
+    croppedTestGrid.[7] =! "##.#.##."
+
+    countUsed testRows =! 8108
+    
+    let input = "jxqlasbh"
+    let rows = makeRowsForInput input
+
+    printfn "Part 1: %d" (countUsed rows)
+    0

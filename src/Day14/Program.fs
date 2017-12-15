1fb8fed5a058326813e5b7804e0729fba5352b05
Ian Griffiths <ian@interact-sw.co.uk>
2017-12-15 07:08:04 +0000

    Day 15

--- src/Day14/Program.fs
+++ src/Day14/Program.fs
@@ -2,7 +2,6 @@
 open Swensen.Unquote
 
 open KnotHashing
-open TextHandling
 
 let toBinary (digits: int) (value: int) =
     [1..digits]
@@ -39,6 +38,57 @@ let countUsed (grid: (bool seq) seq) =
     grid
     |> Seq.sumBy (Seq.sumBy (fun b -> if b then 1 else 0))
 
+let gridAsCoordinateSet (grid: (bool seq) seq) =
+    grid
+    |> Seq.mapi
+        (fun j row -> row |> Seq.mapi (fun i v -> if v then Some (i, j) else None))
+    |> Seq.collect id
+    |> Seq.choose id
+    |> Seq.fold (fun (s: Set<int*int>) (c: int*int) -> Set.add c s) Set.empty
+
+let getPointsInSameRegion (grid: Set<int*int>) (p: int*int) =
+    Seq.unfold
+        (fun (regionSoFar: Set<int*int>, pointsStillToProcess: Set<int*int>) ->
+            if Set.isEmpty pointsStillToProcess then None
+            else
+                let currentPoint = pointsStillToProcess |> Seq.take 1 |> Seq.exactlyOne
+                let (x, y) = currentPoint
+                let adjacentPoints =
+                    [(x + 1, y); (x - 1, y); (x, y + 1); (x, y - 1)]
+                let pointIsUsed ((x, y): int*int) =
+                    if x < 0 || y < 0 || x >= 128 || y >= 128 then false
+                    else Set.contains (x, y) grid
+                let usedAdjacentPoints = adjacentPoints |> Seq.filter pointIsUsed |> Set.ofSeq
+                let newlyDiscoveredPointsInRegion =
+                    Set.difference usedAdjacentPoints regionSoFar
+                let updatedRegion = Set.union regionSoFar newlyDiscoveredPointsInRegion
+                let updatedPointsToProcess =
+                    pointsStillToProcess
+                    |> Set.remove currentPoint
+                    |> Set.union newlyDiscoveredPointsInRegion
+                Some (updatedRegion, (updatedRegion, updatedPointsToProcess)))
+        (Set.singleton p, Set.singleton p)
+    |> Seq.last
+
+let getRegions (grid: Set<int*int>) =
+    let pointIsNotUsed p = Set.contains p grid |> not
+    seq { for j in [0..127] do for i in [0..127] -> (i, j) }
+    |> Seq.fold
+        (fun (pointToRegion: Map<int*int,int>, nextRegionNumber: int) p ->
+            let pointIsAlreadyInRegion = Map.containsKey p pointToRegion
+            if pointIsAlreadyInRegion || pointIsNotUsed p
+            then (pointToRegion, nextRegionNumber)
+            else
+                let newRegion = getPointsInSameRegion grid p
+                let updatedPointToRegion =
+                    newRegion
+                    |> Seq.fold
+                        (fun (m: Map<int*int, int>) p -> Map.add p nextRegionNumber m)
+                        pointToRegion
+                (updatedPointToRegion, nextRegionNumber + 1))
+        (Map.empty, 1)
+    |> fst
+
 [<EntryPoint>]
 let main argv =
 
@@ -61,9 +111,28 @@ let main argv =
     croppedTestGrid.[7] =! "##.#.##."
 
     countUsed testRows =! 8108
+
+    let testGridAsSet = gridAsCoordinateSet testRows
+    let firstRegion = getPointsInSameRegion testGridAsSet (0,0)
+    printfn "%A" firstRegion
+    let testRegions = getRegions testGridAsSet
+    for j in [0..7] do
+        for i in [0..7] do
+            let columnText:string option = Map.tryFind (i,j) testRegions |> Option.map string
+            printf "%s" ((defaultArg columnText ".").PadLeft(3))
+            printf ""
+        printfn ""
     
     let input = "jxqlasbh"
     let rows = makeRowsForInput input
 
     printfn "Part 1: %d" (countUsed rows)
+
+    let gridAsSet = gridAsCoordinateSet rows
+    let regions = getRegions gridAsSet
+    let regionCount =
+        regions
+        |> Seq.distinctBy (fun x -> x.Value)
+        |> Seq.length
+    printfn "Part 2: %d" regionCount
     0
---
+++ src/Day15/Program.fs
@@ -0,0 +1,81 @@
+﻿open Swensen.Unquote
+
+let factorA = 16807L
+let factorB = 48271L
+
+let genNext factor prev =
+    let x = prev * factor
+    x % 2147483647L
+
+let genNextA = genNext factorA
+let genNextB = genNext factorB
+
+let generateSequence predicate genNext start =
+    Seq.unfold
+        (fun prev ->
+            let next = genNext prev
+            Some (next, next))
+        start
+    |> Seq.filter predicate
+
+let pany _ = true
+
+let generateSequenceA = generateSequence pany genNextA
+let generateSequenceB = generateSequence pany genNextB
+
+let isMultipleOf (n: int64) (v:int64) = (v % n) = 0L
+
+let generateSequenceA2 = generateSequence (isMultipleOf 4L) genNextA
+let generateSequenceB2 = generateSequence (isMultipleOf 8L) genNextB
+
+let generatePairs startA startB =
+    Seq.zip (generateSequenceA startA) (generateSequenceB startB)
+
+let generatePairs2 startA startB =
+    Seq.zip (generateSequenceA2 startA) (generateSequenceB2 startB)
+
+let countMatchesInBottom16Bits count (pairs: (int64 * int64) seq) =
+    pairs
+    |> Seq.take count
+    |> Seq.filter
+        (fun (a, b) ->
+            let ls16A = a % 65536L
+            let ls16B = b % 65536L
+            ls16A = ls16B)
+    |> Seq.length
+
+
+let startA = 591L
+let startB = 393L
+
+[<EntryPoint>]
+let main argv =
+    let testPairs = generatePairs 65L 8921L |> Seq.take 5 |> List.ofSeq
+
+    testPairs.[0] =! (1092455L, 430625591L)
+    testPairs.[1] =! (1181022009L, 1233683848L)
+    testPairs.[2] =! (245556042L, 1431495498L)
+    testPairs.[3] =! (1744312007L, 137874439L)
+    testPairs.[4] =! (1352636452L, 285222916L)
+
+    generatePairs 65L 8921L |> countMatchesInBottom16Bits 5 =! 1
+
+    let testPairs2 = generatePairs2 65L 8921L |> Seq.take 5 |> List.ofSeq
+
+    testPairs2.[0] =! (1352636452L, 1233683848L)
+    testPairs2.[1] =! (1992081072L, 862516352L)
+    testPairs2.[2] =! (530830436L, 1159784568L)
+    testPairs2.[3] =! (1980017072L, 1616057672L)
+    testPairs2.[4] =! (740335192L, 412269392L)
+
+    generatePairs2 65L 8921L |> countMatchesInBottom16Bits 1055 =! 0
+    generatePairs2 65L 8921L |> countMatchesInBottom16Bits 1056 =! 1
+
+    let pairs = generatePairs startA startB
+    let matches = pairs |> countMatchesInBottom16Bits 40000000
+    printfn "Part 1: %d" matches
+
+    let pairs2 = generatePairs2 startA startB
+    let matches2 = pairs2 |> countMatchesInBottom16Bits 5000000
+    printfn "Part 2: %d" matches2
+    0

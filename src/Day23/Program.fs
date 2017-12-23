6e24e55bffe8b86c163e35d848822c5641e43356
Ian Griffiths <ian@interact-sw.co.uk>
2017-12-23 14:36:57 +0000

    Day 23 star 2

--- src/Day23/Program.fs
+++ src/Day23/Program.fs
@@ -114,9 +114,50 @@ let run (instructions: Instruction[]) =
         initialState
 
 
+// Sieve of Eratosthenes from http://fsharpnews.blogspot.co.uk/2010/02/sieve-of-eratosthenes.html
+// Not sure this is working - doesn't seem to producing anything like enough primes
+let primesUpTo (n: int) =
+    let a = ResizeArray<int>(n)
+    a.Add(2)
+    let grow() =
+      let p0 = a.[a.Count-1]+1
+      let b = Array.create p0 true
+      for di in a do
+        let rec loop i =
+          if i<b.Length then
+            b.[i] <- false
+            loop(i+di)
+        let i0 = p0/di*di
+        loop(if i0<p0 then i0+di-p0 else i0-p0)
+      for i=0 to b.Length-1 do
+        if b.[i] then a.Add(p0+i)
+    seq
+        {
+            let mutable i = 0
+            while (a.[i]) <= n do
+                yield a.[i]
+                i <- i + 1
+                if i = a.Count then grow()
+        }
+
+
+let countNonPrimes start skip repeats =
+    let final = start + (skip * repeats)
+    let primesInRange =
+        primesUpTo final
+        |> Seq.filter (fun p -> p >= start)
+        |> Set.ofSeq
+    seq { 0..repeats }
+    |> Seq.filter (fun i -> not <| Set.contains (start + i * 17) primesInRange)
+    |> Seq.length
+
+
 [<EntryPoint>]
 let main argv =
     let program = getEmbeddedRows ()  |> Array.map parseLine
+
+    let finalState = run program |> Seq.last
+    printf "Final state: %A" finalState
 
     let mulCount =
         run program
@@ -132,4 +173,19 @@ let main argv =
         |> Seq.length
 
     printfn "Part 1: %d" mulCount
+
+    // Rather crazily, it turns out that the code we are supplied with
+    // looks at number even spaced by an interval of 17, starting from
+    // 107900 (so it looks at 107900, 107917, 107934, etc) and counts
+    // how many of these are not prime numbers, stopping at some upper
+    // limit, which is 1000 numbers later in our case (17000 higher)
+
+    // With the 'debug' mode (starting with a=1) the start and stop
+    // are both 79, so there is only one number checked, and it's prime,
+    // which is why h=0 after the program completes in debug mode.
+    // Check our fast implementation concurs.
+    countNonPrimes 79 79 0 =! 0
+
+    printfn "Part 2: %d" (countNonPrimes 107900 17 1000)
+
     0
